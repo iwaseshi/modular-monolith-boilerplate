85c162b2198413a814709903564f33a6ee547213
iwaseshi <aaa.1470.333@gmail.com>
2024-06-08 06:40:52 +0900

    feat: cloud run IaC

---
+++ infrastructure/modules/cloudrun.go
@@ -0,0 +1,36 @@
+package modules
+
+import (
+	infrastructure "cdk.tf/go/stack"
+	"github.com/aws/jsii-runtime-go"
+	"github.com/cdktf/cdktf-provider-google-go/google/v13/cloudrunservice"
+	"github.com/cdktf/cdktf-provider-google-go/google/v13/cloudrunserviceiambinding"
+	"github.com/cdktf/cdktf-provider-google-go/google/v13/serviceaccount"
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+)
+
+func NewCloudRun(stack cdktf.TerraformStack, name string, account serviceaccount.ServiceAccount) {
+	cr := cloudrunservice.NewCloudRunService(stack, jsii.String("cloudrun"), &cloudrunservice.CloudRunServiceConfig{
+		Location: jsii.String(infrastructure.Region),
+		Name:     jsii.String(name + "-service"),
+		Template: &cloudrunservice.CloudRunServiceTemplate{
+			Spec: &cloudrunservice.CloudRunServiceTemplateSpec{
+				Containers: []*cloudrunservice.CloudRunServiceTemplateSpecContainers{
+					{
+						Image: jsii.String("gcr.io/modular-monolith-sample/fileservice/app"),
+					},
+				},
+				ServiceAccountName: account.Email(),
+			},
+		},
+	})
+
+	cloudrunserviceiambinding.NewCloudRunServiceIamBinding(stack, jsii.String("cloudrun_iam"), &cloudrunserviceiambinding.CloudRunServiceIamBindingConfig{
+		Service:  cr.Name(),
+		Location: cr.Location(),
+		Role:     jsii.String("roles/run.invoker"),
+		Members: &[]*string{
+			jsii.String("allUsers"),
+		},
+	})
+}
---
+++ infrastructure/services/fileservice/fileservice.go
@@ -0,0 +1,25 @@
+package fileservice
+
+import (
+	"cdk.tf/go/stack/modules"
+	"github.com/aws/jsii-runtime-go"
+	"github.com/cdktf/cdktf-provider-google-go/google/v13/serviceaccount"
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+)
+
+const (
+	ServiceName = "fileservice"
+)
+
+func DeployResources(stack cdktf.TerraformStack) cdktf.TerraformStack {
+
+	account := serviceaccount.NewServiceAccount(stack, jsii.String("app_sa"), &serviceaccount.ServiceAccountConfig{
+		AccountId:   jsii.String(ServiceName + "-account"),
+		DisplayName: jsii.String(ServiceName + " account"),
+	})
+
+	modules.NewStorageBucket(stack, account)
+	modules.NewCloudRun(stack, ServiceName, account)
+
+	return stack
+}
